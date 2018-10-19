2340523e1978948bccb9f3af0b298a5055fb0373
Programeri <lumi_toby@live.com>
2018-10-19 14:24:45 +0200

    "added index to redirect to admin module and user module"

---
+++ index.php
@@ -0,0 +1,302 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml" xmlns:og="http://opengraph.org/schema/"><head>
+<!-- Faqja e marrur nga www.beyonce.com/formation -->
+
+<title>Fundraiser</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<link rel="shortcut icon" href="/wp-content/themes/beyonce_2014/assets/images/favicon.png">
+
+<!-- OG Tags -->
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="Beyonce">
+<meta property="og:title" content="Beyonce">
+<meta property="og:url" content="http://www.beyonce.com">
+<meta property="og:description" content="The Official Website of Beyoncé">
+<meta property="og:image" content="assets/images/og-img.jpg">
+
+<i><em style="display:none;">&nbsp;</em><!-- Facebook Pixel Code -->
+<script src="//pixel.mathtag.com/event/js?01AD=3791_aMorMyuUotUR7sgN66iGYXlHOeydJjbEUOIveTtgVRoYD_tMmw&amp;01RI=CFFFE01FF153EFE&amp;01NA=&amp;mt_id=927695&amp;mt_adid=149635&amp;v1=&amp;v2=&amp;v3=&amp;s1=&amp;s2=&amp;s3=&amp;no_log&amp;mathid_data=%7B%22dv1%22%3A%22TW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzU4LjAuMzAyOS4xMTAgU2FmYXJpLzUzNy4zNg%3D%3D%22%2C%22dv2%22%3A%22NDZmMzUzZmIzYWRkNmYyNGZkMGIwYWNlZDRmZGIwYTk%3D%22%2C%22dv3%22%3A%22NjY1NTE1YzFmOWEyNjI5MmVmNmIxNTAwN2Y3Y2IyN2M%3D%22%2C%22dv4%22%3A%22MTM2Nnw3Njh8MTM2Nnw3Mjh8MjR8fA%3D%3D%22%2C%22dv5%22%3A%22RXVyb3BlL0Jlcmxpbg%3D%3D%22%2C%22dv6%22%3A%22%22%2C%22dv7%22%3A%22MA%3D%3D%22%2C%22dv8%22%3A%22dHJ1ZXx0cnVlfHRydWU%3D%22%2C%22dv9%22%3A%22fGVuLVVTfA%3D%3D%22%2C%22dv10%22%3A%22TW96aWxsYXxOZXRzY2FwZXxXaW4zMnw%3D%22%7D"></script><script type="text/javascript" async="" src="//mathid.mathtag.com/d/i.js"></script><script src="http://connect.facebook.net/signals/config/1454758574736309?v=2.7.12" async=""></script><script async="" src="//connect.facebook.net/en_US/fbevents.js"></script><script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1454758574736309');
+fbq('track', "PageView");</script>
+<noscript>&lt;img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1454758574736309&amp;ev=PageView&amp;noscript=1"
+/&gt;</noscript>
+<!-- End Facebook Pixel Code --><script src="//pixel.mathtag.com/event/js?mt_id=927695&amp;mt_adid=149635&amp;v1=&amp;v2=&amp;v3=&amp;s1=&amp;s2=&amp;s3="></script</script></i><script type="text/javascript" async="" src="/_Incapsula_Resource?SWJIYLWA=2977d8d74f63d7f8fedbea018b7a1d05&amp;ns=1"></script><script async="" src="//www.google-analytics.com/analytics.js"></script><script async="" src="https://ad.atdmt.com/m/a.js;m=11267201150354;cache=0.47563005748768084"></script><script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+*, *::after, *::before {
+  box-sizing: inherit;
+}
+
+body, html {
+  margin: 0;
+  padding: 0;
+  background: white;
+  width: 100%;
+  height: 100%;
+  font-family: helvetica, sans-serif;
+}
+
+body {
+  opacity: 0;
+  -webkit-transition: 3s;
+  -moz-transition: 3s;
+  transition: 3s;
+}
+body.fade-in {
+  opacity: 1;
+}
+
+.buttons {
+  position: fixed;
+  z-index: 1001;
+  top: 1em;
+  left: 1em;
+  width: 100%;
+  text-align: left;
+  font-weight: bold;
+  -webkit-transition: 3s;
+  -moz-transition: 3s;
+  transition: 3s;
+}
+.buttons .links {
+  opacity: 0;
+  pointer-events: none;
+  -webkit-transition: 1s;
+  -moz-transition: 1s;
+  transition: 1s;
+}
+.buttons a {
+  display: block;
+  color: white;
+  text-decoration: none;
+  padding: 0.2em;
+  border: 1px solid transparent;
+  margin: 0.2em 0;
+  font-size: 0.8em;
+  -webkit-transition: 1s;
+  -moz-transition: 1s;
+  transition: 1s;
+}
+.buttons a:hover {
+  border-color: white;
+}
+.buttons .continue {
+  font-size: 2em;
+  position: fixed;
+  top: 0em;
+  right: 0.5em;
+}
+.buttons .hover {
+  color: white;
+  margin-right: 1em;
+  font-size: 2em;
+  float: left;
+  cursor: pointer;
+}
+.buttons .hover:hover .links {
+  pointer-events: auto;
+  opacity: 1;
+}
+
+.selector {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+.selector .side {
+  width: 50%;
+  height: 100%;
+  position: fixed;
+}
+.selector .side .label {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translateY(-50%) translateX(-50%) scale(0.9);
+  -moz-transform: translateY(-50%) translateX(-50%) scale(0.9);
+  -ms-transform: translateY(-50%) translateX(-50%) scale(0.9);
+  -o-transform: translateY(-50%) translateX(-50%) scale(0.9);
+  transform: translateY(-50%) translateX(-50%) scale(0.9);
+  text-align: center;
+  font-size: 1em;
+  -webkit-transition: 0.5s;
+  -moz-transition: 0.5s;
+  transition: 0.5s;
+  -webkit-filter: blur(1px);
+}
+.selector .side .label h3 {
+  font-size: 5vw;
+  margin-bottom: 0.2em;
+  -webkit-transition: 1s;
+  -moz-transition: 1s;
+  transition: 1s;
+}
+.selector .side .label h4 {
+  font-size: 5vw;
+  margin-top: 0;
+  -webkit-transition: 1s;
+  -moz-transition: 1s;
+  transition: 1s;
+}
+.selector .side .label:hover {
+  -webkit-transform: translateY(-50%) translateX(-50%) scale(1);
+  -moz-transform: translateY(-50%) translateX(-50%) scale(1);
+  -ms-transform: translateY(-50%) translateX(-50%) scale(1);
+  -o-transform: translateY(-50%) translateX(-50%) scale(1);
+  transform: translateY(-50%) translateX(-50%) scale(1);
+}
+.selector .side.clean {
+  background: black;
+  color: white;
+}
+.selector .side.dirty {
+  left: 50%;
+  background: white;
+  color: black;
+}
+
+section {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: none;
+}
+
+.logo {
+  background: url("https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-236/title.png") no-repeat center center;
+  background-size: 60% auto;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  top: -5%;
+  left: 0%;
+  position: absolute;
+  z-index: 10;
+  -webkit-transition: 5s;
+  -moz-transition: 5s;
+  transition: 5s;
+  pointer-events: none;
+  display: none;
+}
+
+#video-bg {
+  position: fixed;
+  -webkit-transform: translateX(-50%) translateY(-50%);
+  -moz-transform: translateX(-50%) translateY(-50%);
+  -ms-transform: translateX(-50%) translateY(-50%);
+  -o-transform: translateX(-50%) translateY(-50%);
+  transform: translateX(-50%) translateY(-50%);
+  top: 50%;
+  left: 50%;
+  min-width: 110%;
+  min-height: 110%;
+  width: auto;
+  height: auto;
+  z-index: 1;
+}
+
+@media (max-width: 767px) {
+  .buttons .hover,
+  .buttons .continue {
+    font-size: 1em;
+  }
+
+  .buttons .continue { margin-top: 12px; }
+}
+
+
+</style>
+
+</head>
+
+<body class="fade-in">
+
+<div class="selector"><a href="ADMIN/access/index.php" class="side clean show-clean">
+    <div class="label">
+      <h3>ADMINISTRATOR</h3>
+      <h4>(FUNDRAISER)</h4>
+    </div></a><a href="Ndryshimi/folder0/folder1/index.php" class="side dirty show-dirty">
+    <div class="label">
+      <h3>USER</h3>
+      <h4>(FUNDRAISER)</h4>
+    </div></a></div>
+</section><script>
+  var e = document.createElement("script");
+  e.async = true;
+  e.src = "https://ad.atdmt.com/m/a.js;m=11267201150354;cache=" + Math.random();
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(e, s);
+</script>
+<noscript>
+&lt;iframe
+  style="display:none;"
+  src="https://ad.atdmt.com/m/a.html;m=11267201150354;noscript=1"&gt;
+&lt;/iframe&gt;
+</noscript>
+
+<!-- Segment Pixel - MasterSegmentPixel - DO NOT MODIFY -->
+<script type="text/javascript" src="http://invertise.s3.amazonaws.com/pixeljs/f078d00f-3b61-4bb4-86a7-64ca33767395.js"></script><img src="https://secure.adnxs.com/seg?add=6195672&amp;t=2" width="1" height="1"><img src="https://secure.adnxs.com/seg?add=6195677&amp;t=2" width="1" height="1">
+<!-- End of Segment Pixel -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-30239633-1', 'beyonce.com');
+  ga('send', 'pageview');
+</script>
+
+<script type="text/javascript">
+//<![CDATA[
+(function() {
+var _analytics_scr = document.createElement('script');
+_analytics_scr.type = 'text/javascript'; _analytics_scr.async = true; _analytics_scr.src = '/_Incapsula_Resource?SWJIYLWA=2977d8d74f63d7f8fedbea018b7a1d05&ns=1';
+var _analytics_elem = document.getElementsByTagName('script')[0]; _analytics_elem.parentNode.insertBefore(_analytics_scr, _analytics_elem);
+})();
+// ]]>
+</script>
+
+<script>
+(function () {
+    var cleanUrl, dirtyUrl;
+    cleanUrl = 'https://www.youtube.com/embed/7xwrbrgngUo?rel=0&amp;amp;?modestbranding=1&amp;amp;showinfo=0&amp;amp;autohide=1&amp;amp;autoplay=0&amp;controls=0&amp;vq=hd1080';
+    dirtyUrl = 'https://www.youtube.com/embed/LrCHz1gwzTo?rel=0&amp;amp;?modestbranding=1&amp;showinfo=0&amp;amp;autohide=1&amp;amp;autoplay=0&amp;controls=0&amp;vq=hd1080';
+    setTimeout(function () {
+        return $('body').addClass('fade-in');
+    }, 500);
+    $('.show-clean').on('click', function (e) {
+        $('.clean').find('iframe').attr('src', cleanUrl);
+        $('section.dirty').fadeOut(1000, function () {
+            return $('.dirty').find('iframe').attr('src', '');
+        });
+        return $('section.clean').fadeIn(1000);
+    });
+    $('.show-dirty').on('click', function (e) {
+        $('.dirty').find('iframe').attr('src', dirtyUrl);
+        $('section.clean').fadeOut();
+        return $('section.dirty').fadeIn(1000, function () {
+            return $('.clean').find('iframe').attr('src', '');
+        });
+    });
+}.call(this));
+</script>
+<iframe src="http://pixel.mathtag.com/sync/iframe?mt_uuid=42485927-c72c-4800-8224-c7ee1c15e083&amp;no_iframe=1&amp;exsync=http%3a%2f%2fu3s.mathtag.com%2fsync%2fimg%3fadv%3d149635%26uuid%3d42485927-c72c-4800-8224-c7ee1c15e083&amp;mt_adid=149635" id="mm_sync_back_ground" style="visibility: hidden; display: none;"></iframe><div id="mid-container" style="position: absolute; top: -1000px; left: -1000px;"><canvas width="100" height="100"></canvas></div></body></html>
